1af0df607b7f9624385bb20194a6e0bb586dd0a2
Samuele Di Cori <https://github.com/Hackademy-PT-9/Samuele-ProgettoFinale.git>
2024-01-25 16:14:34 +0100

    Welcome page layout

--- project/resources/views/homepage.blade.php
+++ project/resources/views/homepage.blade.php
@@ -1,28 +1,27 @@
 <x-layout>
     <title>Portfolio - Samuele Di Cori -</title>
 
-    <header id="homepage" class="container px-5 py-5 d-flex flex-column h-100 bg-black my-3">
+    <header id="homepage" class="container px-5 py-5 d-flex flex-column h-100 bg-black text-light">
         <div class="row gx-5 align-items-center justify-content-center">
             <div class="col-12 col-md-6 text-center">
                 <div class="d-flex flex-column align-items-center">
                     <div id="homeImg" style="background-image: url('{{ asset('img/me.jpg') }}')"
-                        class="img-fluid"></div>
+                        class="img-fluid rounded-circle border border-light mb-3"></div>
                     <h2 class="text-uppercase">Samuele Di Cori</h2>
                 </div>
             </div>
 
             <div class="col-12 col-md-6">
-                <div class="text-container">
-                    <div class="badge bg-gradient-primary-to-secondary text-white mb-1">
+                <div class="text-container text-center">
+                    <div class="badge bg-gradient-primary-to-secondary text-dark mb-1">
                         <p class="text-uppercase">Design · Development · Marketing</p>
                     </div>
                     <div class="fs-3 fw-light text-muted">I can help your business to</div>
-                    <h1 class="display-3 fw-bolder mb-5"><span class="text-gradient d-inline">Get online and grow
+                    <h1 class="display-3 fw-bolder mb-4"><span class="text-gradient d-inline">Get online and grow
                             fast</span></h1>
-                    <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-md-start mb-3">
+                    <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-md-center mb-3">
                         <a class="btn btn-primary btn-lg px-5 py-3 me-sm-3 fs-6 fw-bolder" href="resume.html">Resume</a>
-                        <a class="btn btn-outline-danger btn-lg px-5 py-3 fs-6 fw-bolder"
-                            href="https://github.com/RedRibbon990">Projects</a>
+                        <a class="btn btn-outline-danger btn-lg px-5 py-3 fs-6 fw-bolder" href="https://github.com/RedRibbon990">Projects</a>
                     </div>
                 </div>
             </div>
@@ -30,20 +29,19 @@
     </header>
 
     <main>
-        <h2 class="text-center mb-4">Some simple examples</h2>
+        <h2 class="text-center mt-4 py-5">Some simple examples</h2>
         <section class="background-section shadow text-black bg-black d-flex justify-content-center">
             <div class="container bg-black">
-
                 <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                            <img src="{{ asset('img/ex1.webp') }}" class="d-block w-75 mx-auto img-fluid" alt="Immagine 1">
+                            <img src="{{ asset('img/ex1.webp') }}" class="d-block w-75 mx-auto img-thumbnail" alt="Immagine 1">
                         </div>
                         <div class="carousel-item">
-                            <img src="{{asset('img/ex2.webp')}}" class="d-block w-75 mx-auto img-fluid" alt="Immagine 2">
+                            <img src="{{ asset('img/ex2.webp') }}" class="d-block w-75 mx-auto img-thumbnail" alt="Immagine 2">
                         </div>
                         <div class="carousel-item">
-                            <img src="{{asset('img/ex3.webp')}}" class="d-block w-75 mx-auto img-fluid" alt="Immagine 3">
+                            <img src="{{ asset('img/ex3.webp') }}" class="d-block w-75 mx-auto img-thumbnail" alt="Immagine 3">
                         </div>
                     </div>
                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
@@ -66,9 +64,9 @@
                             <h2 class="display-5 fw-bolder"><span class="text-gradient d-inline">About Me</span></h2>
                             <p class="lead fw-light mb-4">My name is Samuele di Cori and I learned how to create
                                 websites and databases to grow your business.</p>
-                            <p class="text-muted">check my profile and find the most suitable service for you. Contact
-                                me for a custom need or specific information. In my portfolio you will find examples and
-                                links of my work, which is constantly improving.</p>
+                            <p class="text-muted">Check my profile and find the most suitable service for you. Contact
+                                me for a custom need or specific information. In my portfolio, you will find examples and
+                                links to my work, which is constantly improving.</p>
                             <div class="d-flex justify-content-center fs-2 gap-4">
                                 <a class="text-gradient" href="#!"><i class="bi bi-twitter"></i></a>
                                 <a class="text-gradient" href="#!"><i class="bi bi-linkedin"></i></a>
@@ -80,5 +78,4 @@
             </div>
         </section>
     </main>
-
 </x-layout>
